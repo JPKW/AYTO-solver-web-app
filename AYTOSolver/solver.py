a2a944aa6123104470627349fe9af519ad206a62
Joerg <joerg.wood@gmail.com>
2020-07-12 16:30:49 +1000

    updated to include exclude lists

--- AYTOSolver/solver.py
+++ AYTOSolver/solver.py
@@ -3,23 +3,30 @@
 
 #setup
 
-lr1 = list(["Francesca","Victoria","Mikala","Kaylen","Emma","Julia","Camille","Alyssa","Nicole","Tori"]) # guess order, round 1
-ir1 = 3 #how many were correct?
-lr2 = list(["Camille","Julia","Mikala","Kaylen","Nicole","Alyssa","Emma","Francesca","Tori","Victoria"])  # guess order, round 2
-ir2 = 3 #how many were correct?
-lr3 = list(["Camille","Nicole","Mikala","Kaylen","Victoria","Francesca","Emma","Alyssa","Tori","Julia"])
-ir3 = 4 
-lr4 = list(["Camille","Emma","Mikala","Kaylen","Victoria","Tori","Nicole","Alyssa","Julia","Francesca"])
-ir4 = 4
-lr5 = list(["Camille","Emma","Mikala","Francesca","Tori","Julia","Victoria","Alyssa","Nicole","Kaylen"])
-ir5 = 4
-lr6 = list(["Camille","Victoria","Mikala","Francesca","Emma","Tori","Kaylen","Alyssa","Julia","Nicole"])
-ir6 = 4
-lr7 = list(["Francesca","Nicole","Mikala","Emma","Kaylen","Tori","Victoria","Alyssa","Julia","Camille"])
-ir7 = 4
+lexn = list(["Melanie","Britni","Kiki","Kiki","Kiki","Kiki"])
+lexp = list([0,2,2,4,5,9])
 
-loL = list([lr1, lr2, lr3, lr4, lr5, lr6, lr7]) #add all the rounds together in a list (of lists)
-loIR = list([ir1, ir2, ir3, ir4, ir5, ir6, ir7]) #and all the correct guesses
+lr1 = list(["Stacey","Kiki","Hannah","Chelsey","Melanie","Britni","Amanda","Cheyenne","Rashida","Kayla"]) # guess order, round 1
+ir1 = 2 #how many were correct?
+lr2 = list(["Chelsey","Kiki","Hannah","Kayla","Melanie","Stacey","Amanda","Cheyenne","Rashida","Britni"])  # guess order, round 2
+ir2 = 0 #how many were correct?
+lr3 = list(["Stacey","Amanda","Kiki","Chelsey","Rashida","Britni","Kayla","Melanie","Cheyenne","Hannah"])
+ir3 = 3 
+lr4 = list(["Amanda","Stacey","Kiki","Chelsey","Hannah","Rashida","Kayla","Britni","Melanie","Cheyenne"])
+ir4 = 2
+lr5 = list(["Stacey","Hannah","Kiki","Chelsey","Cheyenne","Melanie","Britni","Rashida","Amanda","Kayla"])
+ir5 = 2
+lr6 = list(["Stacey","Cheyenne","Amanda","Chelsey","Rashida","Britni","Melanie","Kiki","Hannah","Kayla"])
+ir6 = 3
+lr7 = list(["Rashida","Kayla","Melanie","Chelsey","Britni","Amanda","Stacey","Kiki","Cheyenne","Hannah"])
+ir7 = 3
+lr8 = list(["Stacey","Kiki","Amanda","Chelsey","Rashida","Britni","Melanie","Kayla","Cheyenne","Hannah"])
+ir8 = 3
+lr9 = list(["Stacey","Cheyenne","Rashida","Chelsey","Britni","Kayla","Kiki","Amanda","Melanie","Hannah"])
+ir9 = 2
+
+loL = list([lr1, lr2, lr3, lr4, lr5, lr6, lr7, lr8, lr9]) #add all the rounds together in a list (of lists)
+loIR = list([ir1, ir2, ir3, ir4, ir5, ir6, ir7, ir8, ir9]) #and all the correct guesses
 
 n = len(lr1)
 a = list(lr1)
@@ -30,6 +37,10 @@ ix = 0
 def permute(a, l, r):
     if l == r:
         checker = True
+        for x in range(len(lexn)):
+            if checker == True:#wrong this bit iis wrong
+                if a[lexp[x]] == lexn[x]:
+                    checker = False#wrong this bit iis wrong
         for x in range(len(loL)):
             if checker == True:    
                 ix = 0
@@ -39,7 +50,7 @@ def permute(a, l, r):
                     if a[i] == lr[i]: ix = ix + 1
                 if ix != ir:
                     checker = False
-        if checker == True: return(a)
+        if checker == True: print(a)
     else: 
         for i in range(l, r + 1): 
             a[l], a[i] = a[i], a[l] 
